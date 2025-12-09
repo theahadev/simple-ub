1af2474783275a3abcd5fcc9a95489fcc7ae3c4e
ahathedev <ahathedev@ahathe.dev>
2025-12-09 05:26:55 +0300

    added init.py in init scripts

--- init.bat
+++ init.bat
@@ -3,3 +3,4 @@ python3 -m pip install --upgrade pip
 python3 -m venv venv
 call venv\Scripts\activate.bat
 pip install -r requirements.txt
+python3 init.py
--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo apt update && sudo apt install -y python3-pip
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+python3 init.py
